e08c1dc435b94e41b2736c5a248cb32dd0040ea3
virajwee1 <virajwee@gmail.com>
2020-06-07 17:13:58 +0530

    project initiated

---
+++ src/main/java/com/app/signupapi/SignupApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.signupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignupApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/signupapi/SignupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.signupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
